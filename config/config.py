1895752db91d6726e6f190abb982dbc743a66bdd
Debjyoti <debjyoti381@gmail.com>
2025-04-10 15:45:10 +0530

    get and update feature added

--- config/config.py
+++ config/config.py
@@ -2,5 +2,5 @@ from datetime import timedelta
 class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///project.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=2)
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=20)
     JWT_SECRET_KEY = "python"
--- myapp/views/product_views.py
+++ myapp/views/product_views.py
@@ -16,3 +16,28 @@ def insert_product_view():
             return jsonify(msg="Product inserted successfully")
         return jsonify(msg="Provide valid credentials")
     return jsonify(msg="Provide valid method")
+
+@app.route('/get_one_data/<id>', methods=['GET'])
+@jwt_required()
+def get_one_views(id):
+    if request.method == "GET":
+        data = Products.query.get(id)
+        print(data)
+        return jsonify(msg=f"id = {data.id}, name = {data.name}, price = {data.price}"), 200
+    return jsonify(error="Please provide a valid method"), 405
+
+@app.route("/update/<id>", methods=["PUT"])
+@jwt_required()
+def update_views(id):
+    if request.method == "PUT":
+        data = request.get_json()
+        if data:
+            prod = Products.query.get(id)
+            if prod:
+                prod.name = data['name']
+                prod.price = data['price']
+                db.session.commit()
+                return jsonify(msg="product update successfully"), 201
+            return jsonify(error="product is not available"), 400
+        return jsonify(error="provide valid credentials"), 401
+    return jsonify(error="provide proper method"), 405
